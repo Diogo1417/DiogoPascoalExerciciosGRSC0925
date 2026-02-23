72727cbdeac0e315e4b44d476c3b57771c587b64
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-02-23 00:43:19 -0800

    Ex8

---
+++ ExerciciosIF/Ex8.ps1
@@ -0,0 +1,5 @@
+﻿$notas = 10, 12, 15, 8, 19, 20, 5, 14, 11, 13
+$soma = 0; foreach($n in $notas){$soma += $n}
+$mediaGeral = $soma / 10
+$acima = ($notas | Where-Object {$_ -ge $mediaGeral}).Count
+Write-Host "Média: $mediaGeral | Alunos acima da média: $acima"
