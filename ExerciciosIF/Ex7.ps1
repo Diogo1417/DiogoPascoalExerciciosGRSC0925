9fa45caa3b86f700a350b2685294ed3777bb3b23
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-02-23 02:29:37 -0800

    Ex7

--- ExerciciosIF/Ex7.ps1
+++ ExerciciosIF/Ex7.ps1
@@ -1,4 +1,6 @@
-﻿$n1 = 7; $n2 = 6; $n3 = 9
-$media = (($n1 * 2) + ($n2 * 3) + ($n3 * 5)) / 10
-$status = if ($media -ge 6) { "APROVADO" } else { "REPROVADO" }
-Write-Host "Média: $media - Status: $status"
+﻿Clear-Host
+$n1 = Read-Host "Nota 1 (Peso 2)"; $n2 = Read-Host "Nota 2 (Peso 3)"; $n3 = Read-Host "Nota 3 (Peso 5)"
+$media = (([double]$n1 * 2) + ([double]$n2 * 3) + ([double]$n3 * 5)) / 10
+
+"Média: $media"
+if ($media -ge 6) { "APROVADO" } else { "REPROVADO" }
