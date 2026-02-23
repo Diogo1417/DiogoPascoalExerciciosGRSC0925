105f4e3b254f0e198346bc7ea06eaac64405ec48
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-02-23 04:56:58 -0800

    ExI2

---
+++ ExercicioIF/ExI2.ps1
@@ -0,0 +1,9 @@
+﻿$num1 = [int](Read-Host "NUM1")
+$num2 = [int](Read-Host "NUM2")
+$num3 = [int](Read-Host "NUM3")
+
+$maior = $num1
+if ($num2 -gt $maior) { $maior = $num2 }
+if ($num3 -gt $maior) { $maior = $num3 }
+
+"O maior e: $maior"
