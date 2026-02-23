3a712e9af370d72da07d002fdb645fb3b87604bb
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-02-23 04:28:03 -0800

    ExI1

--- ExercicioIF/ExI1.ps1
+++ ExercicioIF/ExI1.ps1
@@ -1,6 +1,7 @@
-﻿$total = Read-Host "Introduza o total de segundos"
-$h = [Math]::Floor($total / 3600)
-$m = [Math]::Floor(($total % 3600) / 60)
-$s = $total % 60
+﻿$tempo = [int](Read-Host "Segundos")
+
+$h = [int]($tempo / 3600)
+$m = [int](($tempo % 3600) / 60)
+$s = $tempo % 60
 
 "Resultado: $h horas, $m minutos e $s segundos"
