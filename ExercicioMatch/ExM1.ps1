198767c45353859ec1dd6bf40cf7a9529c54825b
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-02-23 01:57:36 -0800

    ExM1

--- ExercicioMatch/ExM1.ps1
+++ ExercicioMatch/ExM1.ps1
@@ -1,6 +1,6 @@
-﻿$dia = "domingo" # Podes alterar para testar
-switch ($dia) {
-    "sabado" { "Fim de semana" }
-    "domingo" { "Fim de semana" }
-    default { "Dia útil" }
+﻿$dia = Read-Host "Introduza o dia da semana"
+switch ($dia.ToLower()) {
+    { $_ -in "segunda", "terca", "quarta", "quinta", "sexta" } { Write-Host "Saída -> Dia útil" }
+    { $_ -in "sabado", "domingo" } { Write-Host "Saída -> Fim de semana" }
+    Default { Write-Host "Entrada inválida" }
 }
