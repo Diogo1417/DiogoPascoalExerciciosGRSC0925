83a59f674778730a0ef922ae5635763ade74b1ff
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-02-23 00:26:18 -0800

    Ex1

---
+++ ExerciciosIF/Ex1.ps1
@@ -0,0 +1,5 @@
+﻿$totalSegundos = 3665
+$horas = [Math]::Floor($totalSegundos / 3600)
+$minutos = [Math]::Floor(($totalSegundos % 3600) / 60)
+$segundos = $totalSegundos % 60
+Write-Host "Resultado: $horas hora, $minutos minuto e $segundos segundos"
