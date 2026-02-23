fc267939036c5f3c5b35d0d2d9d4e275b1bbd33b
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-02-23 05:45:05 -0800

    ExI3

---
+++ ExercicioIF/Exl3.ps1
@@ -0,0 +1,7 @@
+﻿$num1 = [int](Read-Host "Primeiro numero")
+$num2 = [int](Read-Host "Segundo numero")
+$num3 = [int](Read-Host "Terceiro numero")
+
+$lista = $num1, $num2, $num3 | Sort-Object
+
+"Numeros ordenados: $lista"
