c51d007352f70d51fc2b9d3633445bc3978aee1f
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-02-23 03:16:32 -0800

    ExM2

--- ExercicioMatch/ExM2.ps1
+++ ExercicioMatch/ExM2.ps1
@@ -1,8 +1,7 @@
-$nota = [int](Read-Host "Introduza a nota (0-100)")
-
+﻿$nota = [int](Read-Host "Nota (0-100)")
 switch ($nota) {
-    { $nota -ge 90 } { "Saída -> Excelente"; break }
-    { $nota -ge 70 } { "Saída -> Bom"; break }
-    { $nota -ge 50 } { "Saída -> Suficiente"; break }
-    Default          { "Saída -> Insuficiente" }
+    { $nota -ge 90 } { "Excelente"; break }
+    { $nota -ge 70 } { "Bom"; break }
+    { $nota -ge 50 } { "Suficiente"; break }
+    Default          { "Insuficiente" }
 }
