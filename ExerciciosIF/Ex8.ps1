f88876737621ecd126947c45af27beb27097a83d
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-02-23 02:31:56 -0800

    Ex8

--- ExerciciosIF/Ex8.ps1
+++ ExerciciosIF/Ex8.ps1
@@ -1,5 +1,11 @@
-﻿$notas = 10, 12, 15, 8, 19, 20, 5, 14, 11, 13
-$soma = 0; foreach($n in $notas){$soma += $n}
-$mediaGeral = $soma / 10
-$acima = ($notas | Where-Object {$_ -ge $mediaGeral}).Count
-Write-Host "Média: $mediaGeral | Alunos acima da média: $acima"
+﻿Clear-Host
+"Introduzir 10 valores separados por vírgula (Ex: 10, 20, 30):"
+$entrada = Read-Host
+$notas = $entrada.Split(',') | ForEach-Object { [double]$_ }
+
+$soma = 0; foreach($n in $notas){ $soma += $n }
+$media = $soma / $notas.Count
+$acima = ($notas | Where-Object { $_ -ge $media }).Count
+
+"Média Geral: $media"
+"Alunos acima da média: $acima"
