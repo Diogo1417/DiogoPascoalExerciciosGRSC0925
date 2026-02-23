d9d655ca1e1b88c972af0e69fde10e5c3e9c72e7
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-02-23 02:00:09 -0800

    ExM4

--- ExercicioMatch/ExM4.ps1
+++ ExercicioMatch/ExM4.ps1
@@ -1,8 +1,7 @@
-﻿$valor = @(10, 20, 30)
-switch ($valor.GetType().Name) {
-    "Int32"   { "Número inteiro" }
-    "Double"  { "Número decimal" }
-    "String"  { "String textual" }
-    "Object[]" { "Lista" }
-    default   { "Tipo desconhecido" }
+﻿$entrada = Read-Host "Introduza um valor (ex: 10, 20, 30 para Lista)"
+switch ($entrada) {
+    { $_ -match ',' } { Write-Host "Saída -> Lista" }
+    { $_ -match '^\d+$' } { Write-Host "Saída -> Número inteiro" }
+    { $_ -match '^\d+[\.,]\d+$' } { Write-Host "Saída -> Número decimal" }
+    Default { Write-Host "Saída -> String textual ou desconhecido" }
 }
