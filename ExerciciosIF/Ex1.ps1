1a1e283524667b2a806c231adcab17b75b28c59b
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-02-23 02:56:48 -0800

    Limpeza de ficheiros antigos

--- ExerciciosIF/Ex1.ps1
+++
@@ -1,7 +0,0 @@
-﻿Clear-Host
-$total = Read-Host "Introduza o total de segundos"
-$h = [Math]::Floor($total / 3600)
-$m = [Math]::Floor(($total % 3600) / 60)
-$s = $total % 60
-
-"Resultado: $h horas, $m minutos e $s segundos"
