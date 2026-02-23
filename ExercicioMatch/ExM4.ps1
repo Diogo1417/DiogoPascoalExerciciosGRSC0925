9a88293d153da882f2ddd07b32ccd3e537f5f18d
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-02-23 01:44:14 -0800

    ExM4

---
+++ ExercicioMatch/ExM4.ps1
@@ -0,0 +1,8 @@
+﻿$valor = @(10, 20, 30)
+switch ($valor.GetType().Name) {
+    "Int32"   { "Número inteiro" }
+    "Double"  { "Número decimal" }
+    "String"  { "String textual" }
+    "Object[]" { "Lista" }
+    default   { "Tipo desconhecido" }
+}
