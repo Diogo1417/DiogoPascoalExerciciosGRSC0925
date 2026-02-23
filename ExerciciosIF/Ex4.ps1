bcf77a53aad7843ca6f9a126ecfc7f7b33f584d2
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-02-23 00:36:00 -0800

    Ex4

---
+++ ExerciciosIF/Ex4.ps1
@@ -0,0 +1,7 @@
+﻿$saldo = 500; $cheque = 300
+if ($cheque -le $saldo) {
+    $saldo -= $cheque
+    Write-Host "Cheque descontado, saldo atual: $saldo"
+} else {
+    Write-Host "Saldo insuficiente para o cheque de $cheque"
+}
