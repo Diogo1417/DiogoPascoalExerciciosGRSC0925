db3da989d964fa1fda193ceaba99e3c3bc8b5225
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-02-23 02:20:53 -0800

    ExM10

---
+++ ExercicioMatch/ExM10.ps1
@@ -0,0 +1,10 @@
+﻿Clear-Host
+$j1 = (Read-Host "Jogador 1 (pedra/papel/tesoura)").ToLower()
+$j2 = (Read-Host "Jogador 2 (pedra/papel/tesoura)").ToLower()
+
+switch ("$j1-$j2") {
+    { $_ -in "pedra-tesoura", "tesoura-papel", "papel-pedra" } { "Jogador 1 venceu"; break }
+    { $_ -in "tesoura-pedra", "papel-tesoura", "pedra-papel" } { "Jogador 2 venceu"; break }
+    { $j1 -eq $j2 } { "Empate"; break }
+    Default { "Jogada inválida" }
+}
