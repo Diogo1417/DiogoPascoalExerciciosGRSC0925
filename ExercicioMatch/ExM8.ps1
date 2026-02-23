76e1665eef4588ce119d1230d2d908509b056c1e
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-02-23 04:14:45 -0800

    ExM8

--- ExercicioMatch/ExM8.ps1
+++ ExercicioMatch/ExM8.ps1
@@ -1,14 +1,13 @@
-﻿Clear-Host
-$op = Read-Host "Operação (soma/subtrai/multiplica/divide)"
-$n1 = Read-Host "Número 1"
-$n2 = Read-Host "Número 2"
+﻿$conta = (Read-Host "Operacao (soma, subtração, multiplicação, divisão)").ToLower()
+$num1 = [int](Read-Host "Primeiro numero")
+$num2 = [int](Read-Host "Segundo numero")
 
-switch ($op.ToLower()) {
-    "soma"       { ([double]$n1 + [double]$n2); break }
-    "subtrai"    { ([double]$n1 - [double]$n2); break }
-    "multiplica" { ([double]$n1 * [double]$n2); break }
-    "divide"     { 
-        if ([double]$n2 -eq 0) { "Erro: Divisão por zero"; break }
-        ([double]$n1 / [double]$n2); break 
+switch ($conta) {
+    "soma"       { $num1 + $num2; break }
+    "subtração"    { $num1 - $num2; break }
+    "multiplicação" { $num1 * $num2; break }
+    "divisão"     { 
+        if ($num2 -eq 0) { "Nao pode dividir por zero"; break }
+        $num1 / $num2; break 
     }
 }
