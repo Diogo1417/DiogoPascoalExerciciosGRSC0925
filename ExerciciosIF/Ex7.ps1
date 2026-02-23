a40f9900744cca56a39c3ff046c2fab479087ca2
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-02-23 00:42:17 -0800

    Ex7

---
+++ ExerciciosIF/Ex7.ps1
@@ -0,0 +1,4 @@
+﻿$n1 = 7; $n2 = 6; $n3 = 9
+$media = (($n1 * 2) + ($n2 * 3) + ($n3 * 5)) / 10
+$status = if ($media -ge 6) { "APROVADO" } else { "REPROVADO" }
+Write-Host "Média: $media - Status: $status"
