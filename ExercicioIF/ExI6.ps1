a7e58d79efb34e947e71178bb145e307314f8177
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-02-23 07:00:55 -0800

    ExI6

---
+++ ExercicioIF/ExI6.ps1
@@ -0,0 +1,11 @@
+﻿$valor = [int](Read-Host "Valor da compra")
+if ($valor -le 200) { 
+    $total = $valor * 0.9 
+} 
+elseif ($valor -le 500) { 
+    $total = $valor * 0.85 
+} 
+else { 
+    $total = $valor * 0.8 
+}
+"Total com desconto: $total"
