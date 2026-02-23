b8de857f40bfcb9ca0989dcc354db311e79240fe
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-02-23 00:30:02 -0800

    Ex2

---
+++ ExerciciosIF/Ex2.ps1
@@ -0,0 +1,4 @@
+﻿$num1 = 5; $num2 = 2; $num3 = 8
+$maior = ($num1, $num2, $num3 | Measure-Object -Maximum).Maximum
+$menor = ($num1, $num2, $num3 | Measure-Object -Minimum).Minimum
+Write-Host "Maior: $maior | Menor: $menor"
