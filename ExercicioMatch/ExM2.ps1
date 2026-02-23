d3a340b891a0ba15a02a3b27f195ae3f4f13e6d3
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-02-23 01:58:29 -0800

    ExM2

--- ExercicioMatch/ExM2.ps1
+++ ExercicioMatch/ExM2.ps1
@@ -1,7 +1,7 @@
-﻿$nota = 85
-switch ($nota) {
-    { $_ -ge 90 } { "Excelente" }
-    { $_ -ge 70 } { "Bom" }
-    { $_ -ge 50 } { "Suficiente" }
-    default { "Insuficiente" }
+﻿$nota = Read-Host "Introduza a nota (0-100)"
+switch ([int]$nota) {
+    { $_ -ge 90 } { Write-Host "Saída -> Excelente" }
+    { $_ -ge 70 } { Write-Host "Saída -> Bom" }
+    { $_ -ge 50 } { Write-Host "Saída -> Suficiente" }
+    Default { Write-Host "Saída -> Insuficiente" }
 }
