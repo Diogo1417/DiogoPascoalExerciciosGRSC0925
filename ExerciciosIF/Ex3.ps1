dac291e6be15b7b662a7cd2a5c71bd02937e3648
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-02-23 00:31:50 -0800

    Ex3

---
+++ ExerciciosIF/Ex3.ps1
@@ -0,0 +1,4 @@
+﻿$num1 = 7; $num2 = 2
+$lista = $num1, $num2
+Write-Host "Crescente: $(($lista | Sort-Object) -join ', ')"
+Write-Host "Decrescente: $(($lista | Sort-Object -Descending) -join ', ')"
