03cfecb3d34b697ba375c92c4581927fb0426993
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-02-23 00:40:06 -0800

    Ex6

---
+++ ExerciciosIF/Ex6.ps1
@@ -0,0 +1,6 @@
+﻿$cliente = "João"; $compra = 350
+if ($compra -le 200) { $desc = 0.10 }
+elseif ($compra -le 500) { $desc = 0.15 }
+else { $desc = 0.20 }
+$valorDesc = $compra * $desc
+Write-Host "Cliente: $cliente | Desconto: $valorDesc | Total: $($compra - $valorDesc)"
