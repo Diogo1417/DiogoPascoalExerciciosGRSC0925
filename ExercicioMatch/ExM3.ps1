ca896934d4ff3c29bb78ff3bb33aed73083c2cde
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-02-23 03:31:08 -0800

    ExM3

--- ExercicioMatch/ExM3.ps1
+++ ExercicioMatch/ExM3.ps1
@@ -1,8 +1,7 @@
-$tipo = Read-Host "Tipo de pedido (compra/venda)"
-$valor = Read-Host "Valor em €"
-
-switch ($tipo.ToLower()) {
-    "compra" { "Compra de $($valor)€"; break }
-    "venda"  { "Venda de $($valor)€"; break }
-    Default  { "Pedido desconhecido" }
+$tipo = (Read-Host "Tipo (compra/venda)").ToLower()
+$valor = Read-Host "Valor"
+switch ($tipo) {
+    "compra" { "Compra de $valor euros"; break }
+    "venda"  { "Venda de $valor euros"; break }
+    Default  { "Desconhecido" }
 }
