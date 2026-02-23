bae315ce4219dc4457c3b93bc64c79c53d630e3b
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-02-23 01:43:25 -0800

    ExM3

---
+++ ExercicioMatch/ExM3.ps1
@@ -0,0 +1,6 @@
+﻿$pedido = @{ tipo = "venda"; valor = 250 }
+switch ($pedido.tipo) {
+    "compra" { "Compra de $($pedido.valor)€" }
+    "venda"  { "Venda de $($pedido.valor)€" }
+    default  { "Pedido desconhecido" }
+}
