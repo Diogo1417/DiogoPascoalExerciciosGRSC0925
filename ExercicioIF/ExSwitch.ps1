4357331a521acedd9142c87b8a594e215c008462
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-02-26 03:44:01 -0800

    ExSwitch

---
+++ ExercicioIF/ExSwitch.ps1
@@ -0,0 +1,17 @@
+﻿$mes = [int](Read-Host "Introduzir o numero do mes")
+
+switch ($mes) {
+    1  { "Janeiro" }
+    2  { "Fevereiro" }
+    3  { "Março" }
+    4  { "Abril" }
+    5  { "Maio" }
+    6  { "Junho" }
+    7  { "Julho" }
+    8  { "Agosto" }
+    9  { "Setembro" }
+    10 { "Outubro" }
+    11 { "Novembro" }
+    12 { "Dezembro" }
+    Default { "Mês inválido" }
+}
