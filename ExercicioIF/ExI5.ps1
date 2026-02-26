b22247865a9145eff9ad3f3c32c9579174cdee50
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-02-26 02:57:39 -0800

    ExI5

--- ExercicioIF/ExI5.ps1
+++ ExercicioIF/ExI5.ps1
@@ -1,10 +1,10 @@
-﻿$n1 = [int](Read-Host "NUM1")
-$n2 = [int](Read-Host "NUM2")
-$n3 = [int](Read-Host "NUM3")
+﻿$num1 = [int](Read-Host "NUM1")
+$num2 = [int](Read-Host "NUM2")
+$num3 = [int](Read-Host "NUM3")
 
-if ($n1 -gt $n2) { $n1, $n2 = $n2, $n1 }
-if ($n2 -gt $n3) { $n2, $n3 = $n3, $n2 }
-if ($n1 -gt $n2) { $n1, $n2 = $n2, $n1 }
+if ($num1 -gt $num2) { $num1, $num2 = $num2, $num1 }
+if ($num2 -gt $num3) { $num2, $num3 = $num3, $num2 }
+if ($num1 -gt $num2) { $num1, $num2 = $num2, $num1 }
 
-"Crescente: $n1 $n2 $n3"
-"Decrescente: $n3 $n2 $n1"
+"Crescente: $num1 $num2 $num3"
+"Decrescente: $num3 $num2 $num1"
