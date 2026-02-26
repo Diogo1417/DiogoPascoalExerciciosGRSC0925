1f21c957fb7ca719c2bbfc570ce78c304632c503
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-02-26 03:10:43 -0800

    ExI6

--- ExercicioIF/ExI6.ps1
+++ ExercicioIF/ExI6.ps1
@@ -1,11 +1,18 @@
-﻿$valor = [int](Read-Host "Valor da compra")
-if ($valor -le 200) { 
-    $total = $valor * 0.9 
-} 
-elseif ($valor -le 500) { 
-    $total = $valor * 0.85 
-} 
-else { 
-    $total = $valor * 0.8 
+﻿$nome = [string](Read-Host "Introduzir nome")
+$compra = [float](Read-Host "Valor da compra")
+$desconto = 0.0
+$total = 0.0
+
+
+if ($compra -le 200) {
+    $desconto = $compra * 0.10
 }
-"Total com desconto: $total"
+elseif ($compra -gt 200 -and $compra -le 500) {
+    $desconto = $compra * 0.15
+}
+else {
+    $desconto = $compra * 0.20
+}
+$total = $compra - $desconto
+
+Write-Host "Nome: $nome | Compra: $compra | Desconto: $desconto | Total a pagar: $total"
