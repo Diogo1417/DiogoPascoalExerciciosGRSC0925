d875b0403ff7d642a55077e33f88b0a2ce696821
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-02-26 03:39:56 -0800

    ExI8

---
+++ ExercicioIF/ExI8.ps1
@@ -0,0 +1,23 @@
+﻿$contador = 0
+$soma = 0
+$notas = @() 
+
+while ($contador -lt 10) {
+    $n = [int](Read-Host "Introduza a nota $($contador + 1)")
+    $notas += $n
+    $soma += $n
+    $contador++
+}
+
+$media = $soma / 10
+$acimaMedia = 0
+$posicao = 0
+
+while ($posicao -lt 10) {
+    if ($notas[$posicao] -ge $media) {
+        $acimaMedia++
+    }
+    $posicao++
+}
+
+Write-Host "Media: $media | Alunos acima ou dentro da media: $acimaMedia"
