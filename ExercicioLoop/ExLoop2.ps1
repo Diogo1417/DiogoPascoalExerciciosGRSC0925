fabe5f2c5251481ba8e5ded3d6a3e556a63198ad
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-03-02 07:09:49 -0800

    ExLoop2

---
+++ ExercicioLoop/ExLoop2.ps1
@@ -0,0 +1,13 @@
+﻿$i=1
+
+while ($i -le 10){
+    $num = Read-Host ("Introduz o $i numero: ")
+    if ($i % 2 -eq 0){
+    Write-Host "$i é par"
+    }
+    else {
+    Write-Host "$i é impar"
+    }
+    $i++
+}
+
