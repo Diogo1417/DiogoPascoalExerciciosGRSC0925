946e6602cba5204a26a85608f5159f1479b9806c
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-03-02 07:37:03 -0800

    Ex3Loop

---
+++ ExercicioLoop/ExLoop3.ps1
@@ -0,0 +1,11 @@
+﻿$i = 1
+[int]$soma = 0 
+
+while ($i -le 10) {
+    [int]$nota = Read-Host "Introduza a nota do $i aluno"
+    $soma = $soma + $nota
+    $i++
+}
+
+$media = $soma / 10
+Write-Host "A média é de: $media"
