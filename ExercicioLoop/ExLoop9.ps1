7ec5a97a2a49c5c801616d6cf3f49450ea26a8e6
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-03-09 08:12:01 -0700

    Ex9Loop

---
+++ ExercicioLoop/ExLoop9.ps1
@@ -0,0 +1,10 @@
+﻿do {
+    $numero = Read-Host "Escreve um número de 1 a 100"
+    
+    if ($numero -lt 1 -or $numero -gt 100) {
+        Write-Host "Inválido $numero"
+    }
+
+} while ($numero -lt 1 -or $numero -gt 100)
+
+Write-Host "Aprovado $numero" 
