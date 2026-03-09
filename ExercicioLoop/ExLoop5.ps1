b7ca80bf97bef14dcd24b8a91bc988df30f7d07f
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-03-09 07:10:10 -0700

    Ex5Loop

--- ExercicioLoop/ExLoop5.ps1
+++ ExercicioLoop/ExLoop5.ps1
@@ -1,7 +1,7 @@
 ﻿$i = 1
 Read-Host "Primir o ENTER para ver 10.000 números"
 
-while ($contagem -le 10000) {
+while ($i -le 10000) {
     Write-Host "Contagem: $i"
     $i++
 }
---
+++ ExercicioLoop/ExLoop6.ps1
@@ -0,0 +1,18 @@
+﻿$num = 2
+$primo = 0
+
+while ($primo -lt 10) {
+    $div = 0
+    $i = 1
+
+    while ($i -le $num) {
+        if ($num % $i -eq 0) { $div++ }
+        $i++
+    }
+
+    if ($div -eq 2) {
+        Write-Host "Primo: $num"
+        $primo++
+    }
+    $num++
+}
