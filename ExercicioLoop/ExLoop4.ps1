5991225f5984cd000383c037345ed24bf51e7460
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-03-09 06:50:58 -0700

    Ex4Loop

---
+++ ExercicioLoop/ExLoop4.ps1
@@ -0,0 +1,17 @@
+﻿for ($vez = 1; $vez -le 5; $vez++) {
+    $num = Read-Host "Diz um numero"
+    $contar = 0
+    $dividir = 1
+
+    while ($dividir -le $num) {
+        if ($num % $dividir -eq 0) { $contar++ }
+        $dividir++
+    }
+
+    if ($contar -eq 2) {
+        Write-Host "O $num é primo! "
+    } else {
+        Write-Host "O $num não é primo."
+    }
+}
+
