d7027933900984a29fa2160cd808a55cdcd75ed6
Diogo Bizarro Pascoal <diogopascoal2003@outlook.com>
2026-03-09 07:15:59 -0700

    Ex6Loop

---
+++ ExercicioLoop/ExLoop7.ps1
@@ -0,0 +1,7 @@
+﻿$num = 10
+Read-Host "Primir o ENTER para ver os números de 10 em 10"
+
+while ($num -le 1000) {
+    Write-Host "Contagem: $num"
+    $num+=10
+}
---
+++ ExercicioLoop/ExLoop8.ps1
@@ -0,0 +1,15 @@
+﻿$num = 10
+$num2 = 15
+Read-Host "Primir o ENTER para ver os números de 10 em 10"
+
+while ($num -le 1000) {
+    Write-Host "Contagem: $num"
+    $num+=10
+}
+
+Read-Host "Primir o ENTER para ver os números de 15 em 15"
+
+while ($num2 -le 995) {
+    Write-Host "Contagem: $num2"
+    $num2+=10
+}
